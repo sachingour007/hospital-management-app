e4b1bbe3f6615fa2ce13b44d85f492e9c080984d
sachingour <gour.sachin.sg@gmail.com>
2024-07-23 23:27:37 +0530

    user login code

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -47,4 +47,33 @@ const patientRegister = asyncHandler(async (req, res, next) => {
     .json(new ApiResponse(200, userCreated, "User registered successfully"));
 });
 
-module.exports = { patientRegister };
+const login = asyncHandler(async (req, res, next) => {
+  const { email, password, role } = req.body;
+
+  if ([email, password, role].some((field) => field.trim() === "")) {
+    return next(new ApiError(400, "Please Provide the all details"));
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return next(new ApiError(400, "Invalid Password or Email"));
+  }
+
+  const isPasswordMatched = await user.isPasswordCorrect(password);
+
+  if (!isPasswordMatched) {
+    return next(new ApiError(400, "Invalid Password or Email"));
+  }
+
+  if (role !== user.role) {
+    return next(new ApiError(400, "User with this Role Not Found"));
+  }
+  const userDetails = await User.findOne(user._id).select("-password");
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, userDetails, "User Logged In Successfully!"));
+});
+
+module.exports = { patientRegister, login };
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { patientRegister } = require("../controllers/user.controller.js");
+const { patientRegister, login } = require("../controllers/user.controller.js");
 
 router.route("/patient/register").post(patientRegister);
+router.route("/login").post(login);
 
 module.exports = router;
