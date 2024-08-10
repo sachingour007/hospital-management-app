b22586bbdebf0df2604908cff9e73e5966e8a1f8
sachingour <gour.sachin.sg@gmail.com>
2024-08-11 01:10:43 +0530

    appointment model

---
+++ server/src/controllers/appointment.controller.js
@@ -0,0 +1,4 @@
+const Appointment = require("../models/appointment.models");
+const asyncHandler = require("../utils/asyncHandler");
+const { ApiError } = require("../utils/ApiError");
+const { ApiResponse } = require("../utils/ApiResponse");
